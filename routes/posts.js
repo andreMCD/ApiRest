95ee31ef8718c0d8d382e942e79e42404dd7b1e5
andre macedo <andredemcd@hotmail.com>
2019-06-07 02:45:25 -0300

    adicionei o get-generico/get-especifico/update/delete

--- routes/posts.js
+++ routes/posts.js
@@ -2,24 +2,62 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("Bem vindo ao post!");
+//RECUPERA TODOS OS POSTS
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-router.post("/", (req, res) => {
+//ENVIA OS POSTS
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description
   });
 
-  post
-    .save()
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json({ message: err });
-    });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//RECUPERANDO POST ESPECÍFICO
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//DELETANDO POST
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.deleteOne({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//ALTERANDO POST
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedpost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedpost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
